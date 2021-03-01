c4496618cda639cf2503a7bdc9a82dac1cfa95eb
Anže Vodovnik <anvod@microsoft.com>
2021-03-01 20:31:19 +0100

    WIP: chore(dotnet): channel generator

--- utils/doclint/generateDotnetChannels.js
+++ utils/doclint/generateDotnetChannels.js
@@ -27,6 +27,24 @@ const { EOL } = require('os');
 const template = fs.readFileSync("./templates/interface.cs", 'utf-8')
   .replace('[PW_TOOL_VERSION]', `${__filename.substring(path.join(__dirname, '..', '..').length).split(path.sep).join(path.posix.sep)}`);
 
+const typeMap = new Map([
+  ["boolean", "bool"],
+  ["boolean?", "bool?"],
+  ["string", "string"],
+  ["string?", "string"],
+  ["number", "float"],
+  ["number?", "float?"],
+]);
+
+const enumValuesMap = new Map([
+  ["undefined", "Undefined"],
+  ["null", "Undefined"],
+  ["-Infinity", "NegativeInfinity"],
+  ["-0", "NegativeZero"]
+]);
+
+const enumMap = new Map();
+
 {
   generateChannels(process.argv[2] || '../generate_types/csharp/channel');
 }
@@ -35,6 +53,24 @@ function generateChannels(location) {
   const yml = fs.readFileSync(path.join(__dirname, '..', '..', 'src', 'protocol', 'protocol.yml'), 'utf-8');
   const protocol = yaml.parse(yml);
 
+  let checkAndMakeDir = (path) => {
+    if (!fs.existsSync(path))
+      fs.mkdirSync(path, { recursive: true });
+  };
+
+  const modelsDir = path.join(location, "models");
+  const channelsDir = path.join(location, "channels");
+  const enumsDir = path.join(location, "enums");
+
+  checkAndMakeDir(modelsDir);
+  checkAndMakeDir(channelsDir);
+  checkAndMakeDir(enumsDir);
+
+  for (const [name, value] of Object.entries(protocol)) {
+    if (value.type === 'object') {
+      typeMap.set(name, name);
+    }
+  }
 
   // const inherits = new Map();
   // // for (const [name, value] of Object.entries(protocol)) {
@@ -47,24 +83,43 @@ function generateChannels(location) {
 
   for (const [name, item] of Object.entries(protocol)) {
     const translatedName = translateMemberName(item.type, name);
-    console.log(`Genearing ${item.type}: ${translatedName}`);
-    renderObject(item.type, item);
+    console.log(`Generating ${item.type}: ${translatedName}`);
+    let out = renderObject(item.type, translatedName, item, name);
+
+    let content = template.replace('[CONTENT]', out.join(`${EOL}\t`));
+    fs.writeFileSync(`${path.join(item.type === "interface" ? channelsDir : modelsDir, translatedName)}.cs`, content);
+  }
+
+  for (const [name, out] of enumMap) {
+    let enumOut = [];
+    enumOut.push(`public enum ${name}`);
+    enumOut.push(`{`);
+    enumOut.push(...out.map(t => `\t${t}`));
+    enumOut.push(`}`);
+    let content = template.replace('[CONTENT]', enumOut.join(`${EOL}\t`));
+    fs.writeFileSync(`${path.join(enumsDir, name)}.cs`, content);
   }
 }
 
 /**
  * 
  * @param {"interface" | "object"} kind 
+ * @param {string} name
  * @param {*} obj 
+ * @param {string} originalName
  */
-function renderObject(kind, obj) {
+function renderObject(kind, name, obj, originalName) {
   let out = [];
+
+  out.push(`// Generated from: ${originalName}`);
+  out.push(`public partial class ${name}`);
+  out.push(`{`);
+
   if (obj.properties)
-    out.push(...renderProperties(kind, obj.properties));
-  let writeFile = (name, out, folder) => {
-    let content = template.replace('[CONTENT]', out.join(`${EOL}\t`));
-    fs.writeFileSync(`${path.join(folder, name)}.cs`, content);
-  }
+    out.push(...renderProperties(kind, obj.properties).map(x => `\t${x}`));
+
+  out.push(`}`);
+  return out;
 }
 
 /**
@@ -75,11 +130,46 @@ function renderObject(kind, obj) {
 function renderProperties(kind, properties) {
   let out = [];
   for (const [name, value] of Object.entries(properties)) {
-    console.log(`Property: ${name} (of type: ${value})`);
+    const propertyName = translateMemberName("property", name);
+    if (out.length != 0)
+      out.push("");
+
+    // get the type
+    const type = translateType(value, propertyName);
+    out.push(`[JsonProperty("${name}")]`);
+    out.push(`public ${type} ${propertyName} { get; set; }`);
   }
   return out;
 }
 
+/**
+ * 
+ * @param {string | object} value 
+ */
+function translateType(value, ownerName) {
+  const isNullable = value[value.length - 1] === '?';
+  if (isNullable)
+    value = value.substring(0, value.length - 1);
+  if (typeof value === 'string') {
+    let mappedType = typeMap.get(value);
+    if (mappedType)
+      return mappedType;
+    throw new Error(`Unknown Type: ${value}`);
+  } else if (typeof value === 'object') {
+    if (value.type && value.type.startsWith('enum')) {
+      if (!ownerName)
+        throw new Error(`Can't generate enum name for ${value}`);
+      let enumName = `${ownerName}Enum`;
+      registerEnum(enumName, value.literals);
+    }
+  }
+}
+
+/**
+ * 
+ * @param {"interface" | "object" | "property" | * } kind 
+ * @param {*} name 
+ */
 function translateMemberName(kind, name) {
   if (!name) return name;
   let assumedName = name.charAt(0).toUpperCase() + name.substring(1);
@@ -89,4 +179,24 @@ function translateMemberName(kind, name) {
     default:
       return assumedName;
   }
+}
+
+/**
+ * 
+ * @param {string} enumName 
+ * @param {string[]} values 
+ */
+function registerEnum(enumName, values) {
+  let out = [];
+  values.forEach(val => {
+    if(val === null) return;
+    let translatedVal = enumValuesMap.get(val) || val;
+    translatedVal = translateMemberName("EnumValue", translatedVal);
+    if (out.length !== 0)
+      out.push("");
+    out.push(`[EnumMember(Value = "${val}")]`);
+    out.push(`${translatedVal},`);
+  });
+
+  enumMap.set(enumName, out);
 }
